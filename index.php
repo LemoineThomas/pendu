d9a586448858bc78f2ecc765c7c663ac933597e7
ThomasLemoine <thomaslemoine@live.fr>
2020-11-24 10:11:19 +0400

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,3 @@
-<!-- Page Lancer la partie 
-
-
-choix du mot 
-deviner une lettre, deviner un mot (modal)
-
-
-enregistrement du mot en session
-
-find des lettres ou comparaison du mot et on renvoi vers l'utilisateur -->
-
 <?php
     session_start();
 
